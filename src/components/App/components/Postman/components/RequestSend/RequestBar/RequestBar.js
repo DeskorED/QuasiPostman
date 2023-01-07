c3cfa1142edeb7efbce2737d91bf669a7d68f7cb
DeskorED <rfpfywtdf29@gmail.com>
2023-01-08 00:59:21 +0100

    new design part

--- src/components/App/components/Postman/components/RequestSend/RequestBar/RequestBar.js
+++ src/components/App/components/Postman/components/RequestSend/RequestBar/RequestBar.js
@@ -2,8 +2,6 @@ import React, {useState} from "react";
 import Button from "../../Button/Button";
 import TextField from "../../TextField/TextField";
 import "./style.scss"
-import {ErrorWarningPanel} from "../ErrorWarningPanel";
-import {RequestService} from "../../../../../../../services/RequestService";
 
 const DEFAULT_URL = "https://rickandmortyapi.com/api"
 
@@ -14,7 +12,6 @@ export function RequestBar({
                                onSendRequest,
                            }) {
     const [url, setUrl] = useState(DEFAULT_URL);
-    const newError = error;
 
     let isValidUrl = urlString => {
         let urlPattern = new RegExp('^(https?:\\/\\/)?' + '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + '((\\d{1,3}\\.){3}\\d{1,3}))' + '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + '(\\?[;&a-z\\d%_.~+=-]*)?' + '(\\#[-a-z\\d_]*)?$', 'i');

dda0d435f69e470308562a0ec71beef3830d487b
DeskorED <rfpfywtdf29@gmail.com>
2023-01-18 20:43:16 +0100

    refactor/: change props of TextField.js component (add: "placeholder" prop)

--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -2,13 +2,20 @@ import React from "react";
 
 import "./style.scss";
 
-export function TextField({ className, onChange, value, readOnly }) {
+export function TextField({
+    placeholder,
+    className,
+    onChange,
+    value,
+    readOnly,
+}) {
     return (
         <input
             className={className}
             onChange={onChange}
             value={value}
             readOnly={readOnly}
+            placeholder={placeholder}
         />
     );
 }
